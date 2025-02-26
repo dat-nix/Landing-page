70b55994fecf0c933294f966671ce9cd0a01557f
dat-nix <dp2838400@gmail.com>
2025-02-26 19:00:16 +0000

    move scoreboard to rikt page

--- src/app/rikt/page.js
+++ src/app/rikt/page.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Scoreboard from "@/components/Scoreboard";
 
 function RIKT() {
-  return <h1>RIKT Page</h1>;
+  return (  
+  <section>
+   <Scoreboard />
+  </section>);
 }
 
 export default RIKT;
