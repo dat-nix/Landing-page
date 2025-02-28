39661a3976a24f9d8785620744779041e1bf5aa4
dat-nix <dp2838400@gmail.com>
2025-02-28 08:32:48 +0000

    update card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ import Image from "next/image";
 
 const Card = ({ image, title, description }) => {
   return (
-    <div className="flex flex-col md:flex-row w-full md:w-[80vw] h-auto md:h-[500px] bg-black overflow-hidden shadow-lg hover:scale-[1.02] transition-transform duration-300 rounded-2xl">
+   <div className="flex flex-col md:flex-row w-full md:w-[80vw] h-auto md:h-[500px] bg-black overflow-hidden shadow-lg hover:scale-[1.02] transition-transform duration-300 p-4 md:p-0">
       {/* Left Image Section */}
-      <div className="w-full md:w-[60%] h-[250px] md:h-auto relative">
+      <div className="w-full md:w-[60%] h-[350px] md:h-auto relative">
         <Image
           src={image}
           alt={title}
@@ -19,7 +19,7 @@ const Card = ({ image, title, description }) => {
       </div>
 
       {/* Right Text Section */}
-      <div className="w-full md:w-[40%] flex flex-col justify-center p-6 md:p-10 bg-black text-white">
+      <div className="w-full md:w-[40%] flex flex-col justify-center p-8 md:p-10 bg-black text-white">
         <h2 className="text-xl md:text-2xl font-bold mb-3 md:mb-4">{title}</h2>
         <p className="text-sm md:text-lg opacity-80">{description}</p>
       </div>
