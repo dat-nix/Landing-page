fcbb5fbb9cc61e208498e462d90dc5bf92601c01
dat-nix <dp2838400@gmail.com>
2025-03-13 15:58:37 +0000

    add language file

---
+++ src/configs/content.jsx
@@ -0,0 +1,24 @@
+export const content = {
+    en: {
+        navbar: {
+            home: "Home",
+            about: "About",
+            rikt: "RMIT Ichigeki Kendo Tournament",
+            contact: "Contact",
+            language: "Tiếng Việt",
+        },
+        // Add other sections here
+    },
+    vn: {
+        navbar: {
+            home: "Trang chủ",
+            about: "Giới thiệu",
+            rikt: "Giải Đấu Kendo RMIT Ichigeki",
+            contact: "Liên hệ",
+            language: "English",
+        },
+        // Add other sections here
+    },
+};
+
+export default content;
