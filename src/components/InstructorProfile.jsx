84bed14cb45c52e57d5071b24e3c52cb3530c77e
SamQT-42 <s4079430@rmit.edu.vn>
2025-03-27 12:06:24 +0700

    Update InstructorProfile.jsx
    Update lần 2 27/3

--- src/components/InstructorProfile.jsx
+++ src/components/InstructorProfile.jsx
@@ -10,17 +10,15 @@ const InstructorProfile = ({ language }) => {
       {/* Layer 1: Gallery Grid with Infinite Slider */}
       <div className="gallery-grid">
         <div className="gallery-track">
-          <div className="gallery-item"><img src="/gallery1.JPG" alt="Gallery 1" /></div>
-          <div className="gallery-item"><img src="/gallery2.png" alt="Gallery 2" /></div>
-          <div className="gallery-item"><img src="/gallery1.JPG" alt="Gallery 3" /></div>
-          <div className="gallery-item"><img src="/gallery2.png" alt="Gallery 4" /></div>
-          <div className="gallery-item"><img src="/gallery1.JPG" alt="Gallery 5" /></div>
-          
-          <div className="gallery-item"><img src="/gallery1.JPG" alt="Gallery 1" /></div>
-          <div className="gallery-item"><img src="/gallery2.png" alt="Gallery 2" /></div>
-          <div className="gallery-item"><img src="/gallery1.JPG" alt="Gallery 3" /></div>
-          <div className="gallery-item"><img src="/gallery2.png" alt="Gallery 4" /></div>
-          <div className="gallery-item"><img src="/gallery1.JPG" alt="Gallery 5" /></div>
+          {[...Array(2)].map((_, i) => (
+            <React.Fragment key={i}>
+              <div className="gallery-item"><img src="/gallery1.JPG" alt="Gallery 1" className="h-[80px] md:h-[150px]" /></div>
+              <div className="gallery-item"><img src="/gallery2.png" alt="Gallery 2" className="h-[80px] md:h-[150px]" /></div>
+              <div className="gallery-item"><img src="/gallery1.JPG" alt="Gallery 3" className="h-[80px] md:h-[150px]" /></div>
+              <div className="gallery-item"><img src="/gallery2.png" alt="Gallery 4" className="h-[80px] md:h-[150px]" /></div>
+              <div className="gallery-item"><img src="/gallery1.JPG" alt="Gallery 5" className="h-[80px] md:h-[150px]" /></div>
+            </React.Fragment>
+          ))}
         </div>
       </div>
 
@@ -29,22 +27,22 @@ const InstructorProfile = ({ language }) => {
         <img 
           src="../profile.png" 
           alt="Instructor Profile" 
-          className="mockup-image"
+           className="mockup-image w-[150px] md:w-[300px]"
         />
       </div>
 
-      {/* Layer 3: Text Overlay */}
-      <div className="text-overlay">
-         <div className="left-orange-text">
-         <h1>OH</h1>
-        <h1>JAE</h1>
-        <h1>HUN</h1>
-    </div>
-        <div className="right-black-text">
-          <p>{instructor.rank}</p>
-          <p>{instructor.years}</p>
-          <p>{instructor.president}</p>
-          <p>{instructor.viceCoach}</p>
+       {/* Layer 3: Text Overlay */}
+       <div className="text-overlay scale-75 md:scale-100">
+        <div className="left-orange-text">
+          <h1 className="text-3xl md:text-6xl">OH</h1>
+          <h1 className="text-3xl md:text-6xl">JAE</h1>
+          <h1 className="text-3xl md:text-6xl">HUN</h1>
+        </div>
+        <div className="right-black-text absolute bottom-0 md:bottom-4" style={{ transform: 'translateY(20px)' }}>
+          <p className="text-3x1 sm md:text-4x1">{instructor.rank}</p>
+          <p className="text-3x1 sm md:text-4x1">{instructor.years}</p>
+          <p className="text-3x1 sm md:text-4x1">{instructor.president}</p>
+          <p className="text-3x1 sm md:text-4x1">{instructor.viceCoach}</p>
         </div>
       </div>
     </div>
