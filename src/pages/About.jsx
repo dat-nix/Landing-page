0afdb17379c7f5d86f94fe705134445bb1563a69
SamQT-42 <s4079430@rmit.edu.vn>
2025-03-28 08:04:25 +0700

    Update About.jsx
    update 28/3

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,18 +17,18 @@ const About = ({ language }) => {
                     <div className="py-6 lg:py-10 flex flex-col lg:flex-row items-start w-full lg:w-3/4 px-6 lg:pl-12">
                         <img src="/logo-without-name.png" alt="Logo" className="w-[10vh] h-[10vh] lg:w-[15vh] lg:h-[15vh] mb-4" />
                         <h1 className="text-orange-600 font-extrabold leading-tight uppercase">
-                            <span className="block text-4xl lg:text-9xl">RMIT</span>
-                            <span className="block text-4xl lg:text-9xl">SHINSEI</span>
-                            <span className="block text-4xl lg:text-9xl">KENDO</span>
-                            <span className="block text-4xl lg:text-9xl">CLUB</span>
+                            <span className="block text-4xl lg:text-9xl font-[Anton] tracking-[0.15em]">RMIT</span>
+                            <span className="block text-4xl lg:text-9xl font-[Anton] tracking-[0.15em]">SHINSEI</span>
+                            <span className="block text-4xl lg:text-9xl font-[Anton] tracking-[0.15em]">KENDO</span>
+                            <span className="block text-4xl lg:text-9xl font-[Anton] tracking-[0.15em]">CLUB</span>
                         </h1>
                     </div>
                     {/* Right Side: Club Description */}
-                    <div className="w-full lg:w-1/2 px-6 py-6 lg:py-0">
-                        <p className="text-lg lg:text-2xl text-black">
+                    <div className="w-full lg:w-1/2 px-14 py-6 lg:py-0">
+                        <p className="text-lg lg:text-3xl text-black">
                             {langData.description[0]} <strong>{langData.description[1]}</strong> {langData.description[2]} <strong>{langData.description[3]}</strong>
                         </p>
-                        <p className="mt-4 text-lg lg:text-2xl text-black">
+                        <p className="mt-4 text-lg lg:text-3xl text-black">
                             {langData.description[4]} <strong>{langData.description[5]}</strong> {langData.description[6]} <strong>{langData.description[7]}</strong> {langData.description[8]}
                         </p>
                     </div>
@@ -44,12 +44,12 @@ const About = ({ language }) => {
            {/* Instructor Highlight & Latest Updates */}
            <div className="w-full min-h-[150px] sm:min-h-[250px] md:min-h-[400px] mx-auto bg-white relative pt-4 sm:pt-1 mt-[-10px] sm:mt-[-20px]">
                 {/* OUR SEONSAENGNIM (Top-Left) */}
-                <h2 className="absolute top-0 left-4 sm:left-10 md:left-17 text-2xl sm:text-[40px] md:text-[100px] font-bold text-orange-500">
+                <h2 className="absolute top-0 left-4 sm:left-10 md:left-17 text-2xl sm:text-[40px] md:text-[100px] font-bold text-orange-500 ">
                     {instructor.description}
                 </h2>
 
                 {/* LATEST (Bottom-Right) */}
-                <h2 className="absolute bottom-1 sm:bottom-4 right-4 sm:right-8 text-7xl sm:text-[60px] md:text-[190px] font-bold text-[#232323]">
+                <h2 className="absolute bottom-1 sm:bottom-4 right-4 sm:right-8 text-7xl sm:text-[60px] md:text-[190px] font-bold text-[#232323] font-[Anton] tracking-[0.15em]">
                     {instructor.text}
                 </h2>
             </div>
@@ -60,14 +60,14 @@ const About = ({ language }) => {
             {/* Gallery Section */}
             <div className="w-full min-h-screen flex-shrink-0 bg-[#FFCA5A] py-10">
                 <h2 
-                     className="text-[#EB4F0B] text-center font-extrabold text-[48px] sm:text-[80px] md:text-[120px] lg:text-[150px] leading-[96%]" 
-                    style={{ fontFamily: "Uni Sans" }}>
+                     className="text-[#EB4F0B] text-center font-extrabold text-[48px] sm:text-[80px] md:text-[120px] lg:text-[150px] leading-[100%] tracking-[0.15em]" 
+                    style={{ fontFamily: "Anton" }}>
                     {gallery.title}
                 </h2>
 
                 {gallery.sections.map((section, index) => (
                     <div key={index} className="mt-10">
-                        <h2 className="text-2xl font-bold mb-8 text-center">{section.title}</h2>
+                        <h2 className="text-2xl font-bold mb-8 text-center text-orange-500 sm:text-[2rem]">{section.title}</h2>
                         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-7xl mx-auto">
                             {section.images.map((image, imgIndex) => (
                                 <img
